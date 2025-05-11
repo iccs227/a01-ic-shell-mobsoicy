4d9c23b5947c5f5e2c79fe16f1603baabe3e21c4
mobsoicy <keonhee.kim1@gmail.com>
2025-05-11 20:21:05 +0700

    added script mode to ic shell

--- icsh.c
+++ icsh.c
@@ -9,13 +9,33 @@
 
 #define MAX_CMD_BUFFER 255
 
-int main() {
+int main(int argc, char *argv[]) {
     char buffer[MAX_CMD_BUFFER];
     char last_command[MAX_CMD_BUFFER] = ""; // To store the last command
+    FILE *script_file = NULL;
+
+    if (argc == 2) {
+        script_file = fopen(argv[1], "r");
+        if (script_file == NULL) {
+            perror("Error opening script file %s\n");
+            return 1;
+        }
+    }
+
     printf("Starting IC shell\n");
     while (1) {
-        printf("icsh $ ");
-        fgets(buffer, MAX_CMD_BUFFER, stdin);
+        if (script_file) {
+            if (fgets(buffer, MAX_CMD_BUFFER, script_file) == NULL) {
+                fclose(script_file);
+                continue;
+            }
+
+        }
+        else {
+            printf("icsh $ ");
+            fgets(buffer, MAX_CMD_BUFFER, stdin);
+        }
+
         buffer[strcspn(buffer, "\n")] = 0; // Remove newline character
         // for !! command
         if (strcmp(buffer, "!!")==0) {
